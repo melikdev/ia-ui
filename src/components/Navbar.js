f49222abea8046faeddafdb4b1f7aa3f8ae12770
Melik Üneşi <melikdev@gmail.com>
2023-01-19 03:26:30 +0300

    fixed navbar bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,143 +12,148 @@ const Navbar = () => {
   const [toggleHamburger, setToggleHamburger] = useState(false)
 
   return (
-    <nav style={{ height: "150px" }} className="container">
-      <ul className="logo">
-        <li>
-          <Link to="/">
-            <img
-              height="100px"
-              width="110px"
-              className="nav-logo"
-              alt=""
-              src={require("../assets/images/nav-logo.png")}
+    <nav className="container-fluid">
+      <nav style={{ height: "150px" }} className="container">
+        <ul className="logo">
+          <li>
+            <Link to="/">
+              <img
+                height="100px"
+                width="110px"
+                className="nav-logo"
+                alt=""
+                src={require("../assets/images/nav-logo.png")}
+              />
+            </Link>
+          </li>
+        </ul>
+        <ul className="links">
+          <li>
+            <details role="list">
+              <summary role="link">Activities</summary>
+              <ul role="listbox">
+                <li>
+                  <Link to="/events">Events</Link>
+                </li>
+                <li>
+                  <Link to="/retreat">Retreat</Link>
+                </li>
+                <li>
+                  <Link to="/">Trainings</Link>
+                </li>
+                <li>
+                  <Link to="/">Conferences</Link>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <Link to="/library">Library</Link>
+          </li>
+          <li>
+            <Link to="/gallery">Gallery</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/about-us">About Us</Link>
+          </li>
+        </ul>
+        <ul className="social-links">
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.instagram.com/islamicagroecology/"
+            >
+              <InstagramIcon />
+            </a>
+          </li>
+        </ul>
+        <div className="hamburger">
+          <li className="hamburger-icon">
+            <MenuIcon
+              style={{ fontSize: "30px" }}
+              onClick={() => setToggleHamburger(!toggleHamburger)}
             />
-          </Link>
-        </li>
-      </ul>
-      <ul className="links">
-        <li>
-          <details role="list">
-            <summary role="link">Activities</summary>
-            <ul role="listbox">
-              <li>
-                <Link to="/events">Events</Link>
-              </li>
-              <li>
-                <Link to="/retreat">Retreat</Link>
-              </li>
-              <li>
-                <Link to="/">Trainings</Link>
-              </li>
-              <li>
-                <Link to="/">Conferences</Link>
-              </li>
-            </ul>
-          </details>
-        </li>
-        <li>
-          <Link to="/library">Library</Link>
-        </li>
-        <li>
-          <Link to="/gallery">Gallery</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/about-us">About Us</Link>
-        </li>
-      </ul>
-      <ul className="social-links">
-        <li>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.instagram.com/islamicagroecology/"
-          >
-            <InstagramIcon />
-          </a>
-        </li>
-      </ul>
-      <div className="hamburger">
-        <li className="hamburger-icon">
-          <MenuIcon
-            style={{ fontSize: "30px" }}
-            onClick={() => setToggleHamburger(!toggleHamburger)}
-          />
-        </li>
-        {toggleHamburger && (
-          <div className="hamburger-menu">
-            <ul>
-              <li>
-                <Link to="/">
-                  <img
-                    alt=""
-                    height="150px"
-                    width="140px"
-                    src={require("../assets/images/nav-logo.png")}
-                  />
-                </Link>
-              </li>
-            </ul>
-            <ul>
-              <li>
-                <details role="list">
-                  <summary role="link">Activities</summary>
-                  <ul role="listbox">
-                    <li>
-                      <Link
-                        onClick={() => setToggleHamburger(false)}
-                        to="/events"
-                      >
-                        Events
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        onClick={() => setToggleHamburger(false)}
-                        to="/retreat"
-                      >
-                        Retreat
-                      </Link>
-                    </li>
-                    <li>
-                      <Link onClick={() => setToggleHamburger(false)} to="/">
-                        Trainings
-                      </Link>
-                    </li>
-                    <li>
-                      <Link onClick={() => setToggleHamburger(false)} to="/">
-                        Conferences
-                      </Link>
-                    </li>
-                  </ul>
-                </details>
-              </li>
-              <li>
-                <Link onClick={() => setToggleHamburger(false)} to="/library">
-                  Library
-                </Link>
-              </li>
-              <li>
-                <Link onClick={() => setToggleHamburger(false)} to="/gallery">
-                  Gallery
-                </Link>
-              </li>
-              <li>
-                <Link onClick={() => setToggleHamburger(false)} to="/contact">
-                  Contact
-                </Link>
-              </li>
-              <li>
-                <Link onClick={() => setToggleHamburger(false)} to="/about-us">
-                  About Us
-                </Link>
-              </li>
-            </ul>
-          </div>
-        )}
-      </div>
+          </li>
+          {toggleHamburger && (
+            <div className="hamburger-menu">
+              <ul>
+                <li>
+                  <Link to="/">
+                    <img
+                      alt=""
+                      height="150px"
+                      width="140px"
+                      src={require("../assets/images/nav-logo.png")}
+                    />
+                  </Link>
+                </li>
+              </ul>
+              <ul>
+                <li>
+                  <details role="list">
+                    <summary role="link">Activities</summary>
+                    <ul role="listbox">
+                      <li>
+                        <Link
+                          onClick={() => setToggleHamburger(false)}
+                          to="/events"
+                        >
+                          Events
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          onClick={() => setToggleHamburger(false)}
+                          to="/retreat"
+                        >
+                          Retreat
+                        </Link>
+                      </li>
+                      <li>
+                        <Link onClick={() => setToggleHamburger(false)} to="/">
+                          Trainings
+                        </Link>
+                      </li>
+                      <li>
+                        <Link onClick={() => setToggleHamburger(false)} to="/">
+                          Conferences
+                        </Link>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+                <li>
+                  <Link onClick={() => setToggleHamburger(false)} to="/library">
+                    Library
+                  </Link>
+                </li>
+                <li>
+                  <Link onClick={() => setToggleHamburger(false)} to="/gallery">
+                    Gallery
+                  </Link>
+                </li>
+                <li>
+                  <Link onClick={() => setToggleHamburger(false)} to="/contact">
+                    Contact
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => setToggleHamburger(false)}
+                    to="/about-us"
+                  >
+                    About Us
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </nav>
     </nav>
   )
 }
