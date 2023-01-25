ccc4c59bdb7c4f4646cf0bd2dcfa7951e395909d
Melik Üneşi <melikdev@gmail.com>
2023-01-25 05:22:27 +0300

    fixed contact page and team component bugs

--- src/components/Team.js
+++ src/components/Team.js
@@ -6,30 +6,32 @@ import useFetch from "../hooks/useFetch"
 const Team = () => {
   const url = process.env.REACT_APP_API_URL + "/teams?populate=*"
   const { data: team } = useFetch(url)
-  console.log(team)
 
   return (
     <main className="container">
-      <section className="grid team">
+      <h1 style={{ textAlign: "center" }}>TEAM</h1>
+      <section className="team grid">
         {team.map((t) => (
-          <article className="member" key={t.id}>
-            <img
-              width="200px"
-              src={
-                process.env.REACT_APP_UPLOAD_URL +
-                t.attributes.member_img.data.attributes.url
-              }
-              alt=""
-            />
-            <section className="desc">
-              <h2>
-                <ReactMarkdown children={t.attributes.member_name} />
-              </h2>
-              <small>
-                <ReactMarkdown children={t.attributes.member_description} />
-              </small>
-            </section>
-          </article>
+          <section key={t.id} className="member-wrapper">
+            <article className="member" key={t.id}>
+              <img
+                width="200px"
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  t.attributes.member_img.data.attributes.url
+                }
+                alt=""
+              />
+              <section className="desc">
+                <h2>
+                  <ReactMarkdown children={t.attributes.member_name} />
+                </h2>
+                <small>
+                  <ReactMarkdown children={t.attributes.member_description} />
+                </small>
+              </section>
+            </article>
+          </section>
         ))}
       </section>
     </main>
